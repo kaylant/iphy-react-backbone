f8a8325e37d46a52ee9c2de7f305728fcc1f0484
Kaylan Smith <kaylansmith@Kaylans-MacBook-Pro.local>
2016-03-23 16:34:52 -0500

    working

--- js/app.js
+++ js/app.js
@@ -44,38 +44,86 @@ function app() {
 		url: "http://api.giphy.com/v1/gifs/search?",
 		_apiKey: "dc6zaTOxFJmzC",
 
-
-
+		parse: function(rawData){
+			// console.log(rawData)
+			return rawData.data
+		}
 	})
 
 	//------------- View -------------//
 	var AppView = React.createClass ({
+
+		componentWillMount: function(){
+			var self = this
+			this.props.gifs.on('sync',function() {self.forceUpdate()})
+		},
+
 		render: function(){
 			// console.log(this)
 			return (
 				<div className="pageContainer">
-					<Scroll gifs = {this.props.gifs.models[0].get('data')}/>
+					<Header/>
+					<SearchBar/>
+					<Scroll gifs={this.props.gifs}/>
 				</div>
 				)
 		}
 	})
 
-	var Scroll = React.createClass ({
+	var HomeView = React.createClass ({
+		render: function(){
+			return(
+				<div className="pageContainer">
+					<Header/>
+					<SearchBar />
+					<p>Welcome to Iphs! Search above.</p>
+				</div>
+				)
+		}
+	})
 
+	var SearchBar = React.createClass ({
+		_search: function(keyEvent) {		
+			if (keyEvent.keyCode === 13) {
+				window.location.hash = `scroll/${keyEvent.target.value}`
+				keyEvent.target.value = ""
+			}
+		},
+
+		render: function(){
+			return <input onKeyDown={this._search} />
+		}
+	})
+
+	var Header = React.createClass({
+		render: function() {
+			return (
+				<div className="titleContainer">
+					<h1 className="pageTitle">Iphy</h1>
+					<h3 className="subTitle">Search iphs</h3>
+				</div>
+				)
+			}
+	})
+
+	var Scroll = React.createClass ({
 		_getGifsJSX: function(objArr){
 			var gifsArr = []
+			var counter = 0
 			objArr.forEach(function(gifObj) {
-				var component = <Gif gif={gifObj} key={gifObj.props} />
+				counter += 1
+				var component = <Gif gif={gifObj} key={counter}/>
 				gifsArr.push(component)
 			})
 			return gifsArr
 		},
 
 		render: function(){
-			// console.log(this)
+			var gifs = this._getGifsJSX(this.props.gifs.models)
+			console.log(gifs.length)
 			return (
 				<div className="gifScroll">
-					{this._getGifsJSX(this.props.gifs)}
+					 {gifs}
 				</div>
 				)
 		}
@@ -85,41 +133,47 @@ function app() {
 		render: function(){
 			// console.log(this)
 			var gifModel = this.props.gif
-			console.log(gifModel)
+			console.log(gifModel.get('slug'))
 			return (
 				<div className="gif">
-					<img src={gifModel.images.original.url}/>
+					<img src={gifModel.get('images').original.url}/>
 				</div>
 				)
 		}
-
 	})
 
 
 	//------------- Router -------------//
 	var IphyRouter = Backbone.Router.extend ({
 		routes: {
-			"scroll/:query" : "handleScrollView",
-			"detail/:id"    : "handleDetailView"
+			"scroll/:query"  : "handleScrollView",
+			"*default"       : "home"
 		},
 
 		handleScrollView: function(query){
-			var coll = new IphyCollection()
-			coll.fetch({
+			this.coll.reset()
+			this.coll.fetch({
 				data: {
 					q: query,
-					"api_key": coll._apiKey
+					"api_key": this.coll._apiKey
 				}
-			}).then(function(){DOM.render(<AppView gifs={coll} />, document.querySelector('.container'))})
+			})
+			DOM.render(<AppView gifs={this.coll} />, document.querySelector('.container'))
+			// console.log(this.coll)		
+		},
+
+		home: function() {
+			location.hash = "home"
+			DOM.render(<HomeView/>, document.querySelector('.container'))
 		},
 
 		initialize: function() {
+			this.coll = new IphyCollection
 			Backbone.history.start()
 		}
 	})
 
 	var rtr = new IphyRouter()
-
     
 }
 
